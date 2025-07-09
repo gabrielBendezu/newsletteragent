3334e087dfa8b5dba4ae1ceda93326e7d8853a1a
gabrielbendezu <gabrielbendezu0101@hotmail.com>
2025-07-09 21:52:52 +0200

    created settings and changed some names

---
+++ settings.py
@@ -0,0 +1,15 @@
+import os
+
+class Settings:
+    """Centralized settings management."""
+    
+    # Embedding settings
+    EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "text-embedding-3-small")
+    EMBEDDING_PROVIDER = os.getenv("EMBEDDING_PROVIDER", "openai")
+    EMBEDDING_DIM = int(os.getenv("EMBEDDING_DIM", "1536"))
+    
+    # Qdrant settings
+    QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY", "")
+    QDRANT_COLLECTION = os.getenv("QDRANT_COLLECTION", "newsletters")
+    
