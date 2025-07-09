9966ebe989a3ad7265b885f70bcad112b549b1e3
gabrielbendezu <gabrielbendezu0101@hotmail.com>
2025-07-09 13:10:27 +0200

    fix no loading env

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 app = create_app()
 
 if __name__ == '__main__':
+    load_dotenv()
+
     host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('DEBUG', 'false').lower() == 'true'
