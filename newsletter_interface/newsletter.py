3a5ed3b5bf147b37a03f95c885fcdf1bc55d596e
gabrielbendezu <gabrielbendezu0101@hotmail.com>
2025-07-09 15:52:53 +0200

    leaner newsletter

--- newsletter_interface/newsletter.py
+++ newsletter_interface/newsletter.py
@@ -4,19 +4,35 @@ from typing import List, Dict
 @dataclass
 class NewsletterEmail:
     message_id: str
-    thread_id: str
     subject: str
     sender: str
-    sender_name: str
-    recipient: str
     date: str
     timestamp: int
     content_plain: str
-    content_html: str
     newsletter_name: str
     article_urls: List[str]
-    labels: List[str]
     snippet: str
     
     def to_dict(self) -> Dict:
         return asdict(self)
+    
+# Comprehensive:
+# @dataclass
+# class NewsletterEmail:
+#     message_id: str
+#     thread_id: str
+#     subject: str
+#     sender: str
+#     sender_name: str
+#     recipient: str
+#     date: str
+#     timestamp: int
+#     content_plain: str
+#     content_html: str
+#     newsletter_name: str
+#     article_urls: List[str]
+#     labels: List[str]
+#     snippet: str
+    
+#     def to_dict(self) -> Dict:
+#         return asdict(self)
