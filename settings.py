af8ed1363270e4afcc2c0980855c5f2b79282f84
gabrielbendezu <gabrielbendezu0101@hotmail.com>
2025-07-09 22:20:53 +0200

    settings stuff

--- settings.py
+++ settings.py
@@ -5,8 +5,9 @@ class Settings:
     
     # Embedding settings
     EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "text-embedding-3-small")
-    EMBEDDING_PROVIDER = os.getenv("EMBEDDING_PROVIDER", "openai")
+    EMBEDDING_PROVIDER = os.getenv("EMBEDDING_PROVIDER", "openai") # ???
     EMBEDDING_DIM = int(os.getenv("EMBEDDING_DIM", "1536"))
+    EMBEDDING_KEY = os.getenv("OPENAI_API_KEY", "")
     
     # Qdrant settings
     QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
