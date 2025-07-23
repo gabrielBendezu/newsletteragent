2f49c9c72786884eeae9157e66ea68f1a92734ec
gabrielbendezu <gabrielbendezu0101@hotmail.com>
2025-07-23 15:53:21 +0200

    Updated config to remove url

--- qdrant_config.py
+++ qdrant_config.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass
 from haystack.utils import Secret
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 @dataclass
 class QdrantConfig:
     """Configuration for Qdrant connection"""
-    url: str = "https://07d9c379-addb-4c4c-a4c6-5a6834c9f9aa.eu-central-1-0.aws.cloud.qdrant.io"
+    url: str = os.getenv("QDRANT_URL", "")
     api_key: Secret = Secret.from_env_var("QDRANT_API_KEY")
     collection_name: str = "newsletter_articles"
     embedding_model: str = "text-embedding-3-small"
